8011f0d66086f7b8d4e64f33dfe1b55bf9c7bd84
Dirk Mika <dirk@mika.gl>
2013-05-08 17:27:24 +0200

    Initial commit

---
+++ src/settings.h
@@ -0,0 +1,20 @@
+//
+//  settings.h
+//  
+//
+//  Created by Dirk Mika on 16.04.13.
+//
+//
+
+#ifndef _settings_h
+#define _settings_h
+
+typedef struct
+{
+    bool dummy;
+} SampleSettings;
+
+void display_settings(SampleSettings *sample_settings);
+
+#endif
+
